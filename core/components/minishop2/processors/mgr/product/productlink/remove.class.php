eb4c20a57868c3eabc27f00e457fcd1d5a19001a
Bezumkin <bezumkin@yandex.ru>
2014-03-08 13:33:46 +0700

    Fixed #111 - remove of products links.

--- core/components/minishop2/processors/mgr/product/productlink/remove.class.php
+++ core/components/minishop2/processors/mgr/product/productlink/remove.class.php
@@ -12,7 +12,7 @@ class msLinkRemoveProcessor extends modObjectRemoveProcessor  {
 		if (!$this->modx->hasPermission($this->permission)) {
 			return $this->modx->lexicon('access_denied');
 		}
-		return parent::initialize();
+		return true;
 	}
 
 
@@ -37,22 +37,24 @@ class msLinkRemoveProcessor extends modObjectRemoveProcessor  {
 		}
 		$type = $msLink->get('type');
 
-		$sql = "DELETE FROM {$this->modx->getTableName('msProductLink')} WHERE `link` = {$link} AND ";
+		$q = $this->modx->newQuery('msProductLink');
+		$q->command('DELETE');
 		switch ($type) {
 			case 'many_to_many':
 			case 'one_to_one':
-				$sql .= "`master` = {$slave} OR `slave` = {$slave}";
+				$q->where(array('master' => $slave, 'OR:slave:=' => $slave));
 			break;
 
 			case 'one_to_many':
-				$sql .= "`master` = {$master}";
+				$q->where(array('master' => $master));
 			break;
 
 			case 'many_to_one':
-				$sql .= "`slave` = {$slave}";
+				$q->where(array('slave' => $slave));
 			break;
 		}
-		$this->modx->exec($sql);
+		$q->prepare();
+		$q->stmt->execute();
 
 		return $this->success('');
 	}
